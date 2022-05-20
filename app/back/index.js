ca4a0d5aedeec43722f6cc5ee7e021d21ec43f50
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-05-20 23:22:48 +0100

    BACKEND: Server running

---
+++ app/back/index.js
@@ -0,0 +1,41 @@
+process.stdout.write("\x1B[2J\x1B[0f");
+
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+
+(async function () {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL || "mongodb://localhost:27017/",
+      {
+        dbName: process.env.MONGO_DB || "policeAcademy",
+      }
+    );
+    console.log("💾 Connected to DB");
+  } catch (err) {
+    throw new Error(`Error connecting to DB: ${err}`);
+  }
+
+  try {
+    const app = express()
+      .use(cors())
+      .use(morgan("dev"))
+      .use(express.json())
+      .use("/api", require("./routes/index"));
+
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.info(">".repeat(40));
+      console.info("🚲   PoliceAcademy Server Live");
+      console.info(`📡  PORT: http://localhost:${PORT}`);
+      console.info(">".repeat(40) + "\n");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();

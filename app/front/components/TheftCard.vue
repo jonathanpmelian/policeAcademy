120def6738d1c3349c2dc7aa6d1e0f0a99124819
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-06-01 23:08:07 +0100

    Fix

--- app/front/components/TheftCard.vue
+++ app/front/components/TheftCard.vue
@@ -46,7 +46,7 @@ export default {
   props: ['data'],
   computed: {
     theftAssigned() {
-      return this.data.assignation
+      return this.data.assignation.department
     },
   },
   methods: {

c6875f5e93b65002aa8320a2c66334632e0ca781
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-06-02 13:47:21 +0100

    ENV: nuxtconfig

--- app/front/nuxt.config.js
+++ app/front/nuxt.config.js
@@ -24,6 +24,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~plugin/maps.js', ssr: true }],
 
+  env: {
+    MAPS_KEY: process.env.MAPS_KEY,
+  },
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
